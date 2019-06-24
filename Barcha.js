a8b28b73ac4fca1168874fee66f2d7962cd004b4
juliachaush <51079330+juliachaush@users.noreply.github.com>
2019-06-24 19:57:01 +0300

    Add files via upload

--- Barcha.js
+++ Barcha.js
@@ -1,12 +1,6 @@
-function hello(){};
-function world(){};
-console.log(hello + world);
+Button.addEventListener ('click', function() {
+     box.style.display === 'none' ? 'block' : 'none';
+} )
 
-// document.getElementById('button-hide').onclick = function() {
-    //  document.getElementById('box').style.display = 'none';
-// }
-document.getElementById('buton-hide').addEventListener ('click', function () {
-    document.getElementById ('box').style.display = 'none';
-})
 
 
